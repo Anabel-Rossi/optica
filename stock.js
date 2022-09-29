a102a061219857b2090436fb37ed1794e8f2c7c6
Anabel rossi rios <anabelrossi40@gmail.com>
2022-09-29 00:55:03 -0300

    arreglos

--- stock.js
+++ stock.js
@@ -1,7 +1,19 @@
 let stockProductos = [
-    {id: 1, nombre: "Glasses Elegant", tipo: "gafas", cantidad: 1, desc: "", precio: 1200, talle: "L", img: '../img/gafas1.jpg'},
-    {id: 2, nombre: "Vich", tipo: "gafas", cantidad: 1, desc: "", precio: 1100, talle: "L", img: '../img/gafas2.jpg'},
-    {id: 3, nombre: "Elegant Sport", tipo: "gafas", cantidad: 1, desc: "", precio: 1200, talle: "M", img: '../img/gafas3.jpg'},
+    {id: 1, nombre: "Glasses Elegant", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas1.jpg'},
+    {id: 2, nombre: "Vich", tipo: "gafas", cantidad: 1, desc: "", precio: 1100,  img: './img/gafas2.jpg'},
+    {id: 3, nombre: "Elegant Sport", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas3.jpg'},
+    {id: 1, nombre: "Glasses Elegant", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas4.jpg'},
+    {id: 2, nombre: "Vich", tipo: "gafas", cantidad: 1, desc: "", precio: 1100,  img: './img/gafas5.jpg'},
+    {id: 3, nombre: "Elegant Sport", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas6.jpg'},
+    {id: 1, nombre: "Glasses Elegant", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas7.jpg'},
+    {id: 2, nombre: "Vich", tipo: "gafas", cantidad: 1, desc: "", precio: 1100,  img: './img/gafas8.jpg'},
+    {id: 3, nombre: "Elegant Sport", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas9.jpg'},
+    {id: 1, nombre: "Glasses Elegant", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas10.jpg'},
+    {id: 2, nombre: "Vich", tipo: "gafas", cantidad: 1, desc: "", precio: 1100,  img: './img/gafas11.jpg'},
+    {id: 3, nombre: "Elegant Sport", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas12.jpg'},
+    {id: 1, nombre: "Glasses Elegant", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas13.jpg'},
+    {id: 2, nombre: "Vich", tipo: "gafas", cantidad: 1, desc: "", precio: 1100,  img: './img/gafas14.jpg'},
+    {id: 3, nombre: "Elegant Sport", tipo: "gafas", cantidad: 1, desc: "", precio: 1200,  img: './img/gafas15.jpg'},
     
 ]
 
