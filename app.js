45afbc96232f448e7a81bf2d827ac2af36d65c63
Anabel rossi rios <anabelrossi40@gmail.com>
2022-10-12 21:30:37 -0300

    fetch

--- app.js
+++ app.js
@@ -145,6 +145,15 @@ boton.addEventListener("click", () => {
 
 }
 
+fetch("./producto.json")
+.then(function(resp) {
+    return resp.json();
+})
+.then(function(data){
+    console.log(data);
+});
+
+
 
 
 
